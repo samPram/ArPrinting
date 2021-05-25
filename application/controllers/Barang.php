b80a0bbaca8d0a27ca096690bc81193e7a100718
Pram4dafi <aldindzikri@gmail.com>
2021-05-25 08:23:10 +0700

    upload image barang

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -48,10 +48,18 @@ class Barang extends CI_Controller
 
         public function add()
         {
+                $config['upload_path'] = './uploads';
+                $config['allowed_types'] = '|jpg|jpeg|png';
+                $config['max_size'] = 2000;
+                $config['file_name'] = 'img-' . date('YmdHis');
+
+                $this->load->library('upload', $config);
+
                 if ($this->session->userdata('level') == 'Admin') {
 
                         $this->form_validation->set_rules('nama', 'Nama', 'required|trim');
                         $this->form_validation->set_rules('satuan', 'Satuan', 'required|trim');
+                        // $this->form_validation->set_rules('image', 'Image', 'callback_file_check');
 
                         if ($this->form_validation->run() == false) {
                                 $this->load->view('template/header');
@@ -60,26 +68,33 @@ class Barang extends CI_Controller
                                 $this->load->view('admin/v_tambahBarang');
                                 $this->load->view('template/footer');
                         } else {
-                                $nama = htmlspecialchars($this->input->post('nama', true));
-                                $satuan = htmlspecialchars($this->input->post('satuan', true));
-                                $data = [
-                                        'nama_produk' => $nama,
-                                        'satuan' => $satuan,
-                                        'quantity' => 0
-                                ];
-
-                                if ($this->M_barang->tambah_data($data) > 0) {
-                                        $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissable">
-                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                                Success inserting data.
-                            </div>');
-                                } else {
+                                if (!$this->upload->do_upload('image')) {
                                         $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissable">
-                                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                                        Failed insert data!
-                                    </div>');
-                                }
+                                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                               ' . $this->upload->display_errors() . '
+                                            </div>');
+                                } else {
+                                        $nama = htmlspecialchars($this->input->post('nama', true));
+                                        $satuan = htmlspecialchars($this->input->post('satuan', true));
+                                        $data = [
+                                                'nama_produk' => $nama,
+                                                'satuan' => $satuan,
+                                                'quantity' => 0,
+                                                'image' => $this->upload->data('file_name')
+                                        ];
 
+                                        if ($this->M_barang->tambah_data($data) > 0) {
+                                                $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissable">
+                                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                        Success inserting data.
+                                    </div>');
+                                        } else {
+                                                $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissable">
+                                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                                Failed insert data!
+                                            </div>');
+                                        }
+                                }
                                 redirect('barang');
                         }
                 } else {
--- application/views/admin/v_tambahBarang.php
+++ application/views/admin/v_tambahBarang.php
@@ -9,20 +9,29 @@
 
       <div class="row">
         <div class="col-sm-12">
-          <form action="<?= base_url(); ?>Barang/add" method="POST">
-            <div class="form-group">
-              <label for="namaProduk">Nama</label>
-              <input type="text" class="form-control" id="nama" name="nama" value="<?= set_value('nama'); ?>">
-              <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
-            </div>
-            <div class="form-group">
-              <label for="satuan">Satuan</label>
-              <select class="form-control" id="satuan" name="satuan" data-style="btn-white">
-                <option value="Lembar">Lembar</option>
-                <option value="Buah">Buah</option>
-              </select>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+
+          <?php //echo $error; 
+          ?>
+
+          <?php echo form_open_multipart('Barang/add', ['method' => 'post']); ?>
+          <!-- <form action="<?= base_url(); ?>Barang/add" method="POST"> -->
+          <div class="form-group">
+            <label for="namaProduk">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama" value="<?= set_value('nama'); ?>">
+            <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
+          </div>
+          <div class="form-group">
+            <label for="satuan">Satuan</label>
+            <select class="form-control" id="satuan" name="satuan" data-style="btn-white">
+              <option value="Lembar">Lembar</option>
+              <option value="Buah">Buah</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="image">Image</label>
+            <input type="file" class="form-control" id="image" name="image">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
           </form>
         </div>
       </div>
